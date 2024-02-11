03b763c352d84b09585361afd06ca8b27a09570f
Mikk0git <kasprzakmilan1@gmail.com>
2024-02-11 21:05:10 +0100

    generationg shortened link

--- src/components/mainContainer/FormLink.tsx
+++ src/components/mainContainer/FormLink.tsx
@@ -1,19 +1,45 @@
+import React, { useState } from "react";
+import supabase from "../supabaseClient";
 import styles from "./MainContainer.module.css";
+
 interface FormFileProps {
   setFinalUrl: React.Dispatch<React.SetStateAction<string>>;
 }
+
 const FormLink: React.FC<FormFileProps> = ({ setFinalUrl }) => {
-  function handleNewLink() {
-    setFinalUrl("link");
+  const [originalUrl, setOriginalUrl] = useState<string>("");
+
+  async function handleNewLink() {
+    if (originalUrl) {
+      const { data, error } = await supabase
+        .from("links")
+        .insert([{ original_url: originalUrl }]) // todo After adding auth add user_id here
+        .select();
+      if (error) {
+        console.error(error);
+      } else {
+        // console.log(data);
+        setFinalUrl(
+          `${import.meta.env.SITE}${import.meta.env.BASE_URL}l/${data[0].id}`,
+        );
+      }
+    }
   }
 
   return (
     <div className={styles.mainForm} id={styles.formLink}>
-      <input type="text" name="" placeholder="URL" />
+      <input
+        type="text"
+        name="original_url"
+        placeholder="URL"
+        value={originalUrl}
+        onChange={(e) => setOriginalUrl(e.target.value)}
+      />
       <button id={styles.submitButton} onClick={handleNewLink}>
         ⚡
       </button>
     </div>
   );
 };
+
 export default FormLink;
